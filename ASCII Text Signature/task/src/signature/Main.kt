58e0cbd88ee518a98f510316fa24d7f91038bb1c
George Samyual <samyual@gmail.com>
2020-05-14 15:06:55 +0300

    Update Main.kt

--- ASCII Text Signature/task/src/signature/Main.kt
+++ ASCII Text Signature/task/src/signature/Main.kt
@@ -1,100 +1,104 @@
 package signature
 
+import java.io.File
+import java.util.*
+import kotlin.properties.Delegates
+
 fun main() {
     print("Enter name and surname: ")
     val name = readLine()!!
     print("Enter person's status: ")
     val status = readLine()!!
 
-    println(VisitCard(name, status))
+    println(Badge(name, status))
 }
 
-class VisitCard(name: String, private val status: String) : Iterable<String> {
+class Badge(name: String, status: String) : Iterable<String> {
 
-    private val tag = Tag(name)
-
-    private val maxWidth by lazy { tag.length.coerceAtLeast(status.length) }
-
-    override fun iterator() = object : Iterator<String> {
-        var lineNo = 0
-        override fun hasNext() = lineNo in 0..5
-        override fun next() = this@VisitCard[lineNo++]
+    private companion object {
+        const val ROMAN = "/home/samyual/Загрузки/roman.txt"
+        const val MEDIUM = "/home/samyual/Загрузки/medium.txt"
     }
+
+    private val nameTag = Tag(name, Font(ROMAN))
+    private val statusTag = Tag(status, Font(MEDIUM))
+    private val width by lazy { nameTag.length.coerceAtLeast(statusTag.length) }
 
     override fun toString() = fold(StringBuilder()) { builder, line -> builder.appendln(line) }.toString()
 
-    private operator fun get(line: Int) = when (line) {
-        0, 5 -> "*".repeat(maxWidth + 6)
-        1, 2, 3 -> "*  ${tag[line - 1].center(maxWidth)}  *"
-        4 -> "*  ${status.center(maxWidth)}  *"
-        else -> throw IndexOutOfBoundsException("Line number is $line, but it must be in range 0..5")
+    override fun iterator() = object : Iterator<String> {
+        val lines = lines().toList()
+        var current = 0
+        override fun hasNext() = current in lines.indices
+        override fun next() = lines[current++]
     }
 
-    private fun String.center(width: Int) = if (length == width) this
-    else {
-        val startPad = (width - length) / 2
-        val endPad = width - length - startPad
-        " ".repeat(startPad) + this + " ".repeat(endPad)
+    private fun lines() = sequence {
+        val firstAndLast = "8".repeat(width + 8)
+        yield(firstAndLast)
+        nameTag.forEach {
+            yield("88  ${it.center(width)}  88")
+        }
+        statusTag.forEach {
+            yield("88  ${it.center(width)}  88")
+        }
+        yield(firstAndLast)
+    }
+
+    private fun String.center(width: Int): String {
+        val startPad = (width - this.length) / 2
+        val endPad = width - this.length - startPad
+        return " ".repeat(startPad) + this + " ".repeat(endPad)
     }
 }
 
-private class Tag(private val text: String) {
+private class Tag(private val text: String, private val font: Font) : Iterable<String> {
 
-    val length: Int by lazy {
-        text.fold(0) { len, ch -> len + Font[ch].width } + text.length.dec()
+    val length: Int by lazy { text.fold(0) { len, ch -> len + font[ch].width } }
+
+    private val height = font.height
+
+    operator fun get(line: Int) = text.map { font[it][line] }.joinToString("")
+
+    override fun iterator() = object : Iterator<String> {
+        var index = 0
+        override fun hasNext() = index < height
+        override fun next() = this@Tag[index++]
     }
-
-    operator fun get(line: Int) = text.map { Font[it][line] }.joinToString(" ")
 }
 
-private object Font {
+private class Font(private val fileName: String) {
 
-    operator fun get(ch: Char) = glyphs[ch.toUpperCase()] ?: SPACE
+    var height by Delegates.notNull<Int>()
+        private set
 
-    private val SPACE = Glyph("    ", "    ", "    ")
-
-    private val glyphs = mapOf(
-            ' ' to SPACE,
-            'A' to Glyph("____", "|__|", "|  |"),
-            'B' to Glyph("___ ", "|__]", "|__]"),
-            'C' to Glyph("____", "|   ", "|___"),
-            'D' to Glyph("___ ", "|  \\", "|__/"),
-            'E' to Glyph("____", "|___", "|___"),
-            'F' to Glyph("____", "|___", "|   "),
-            'G' to Glyph("____", "| __", "|__]"),
-            'H' to Glyph("_  _", "|__|", "|  |"),
-            'I' to Glyph("_", "|", "|"),
-            'J' to Glyph(" _", " |", "_|"),
-            'K' to Glyph("_  _", "|_/ ", "| \\_"),
-            'L' to Glyph("_   ", "|   ", "|___"),
-            'M' to Glyph("_  _", "|\\/|", "|  |"),
-            'N' to Glyph("_  _", "|\\ |", "| \\|"),
-            'O' to Glyph("____", "|  |", "|__|"),
-            'P' to Glyph("___ ", "|__]", "|   "),
-            'Q' to Glyph("____", "|  |", "|_\\|"),
-            'R' to Glyph("____", "|__/", "|  \\"),
-            'S' to Glyph("____", "[__ ", "___]"),
-            'T' to Glyph("___", " | ", " | "),
-            'U' to Glyph("_  _", "|  |", "|__|"),
-            'V' to Glyph("_  _", "|  |", " \\/ "),
-            'W' to Glyph("_ _ _", "| | |", "|_|_|"),
-            'X' to Glyph("_  _", " \\/ ", "_/\\_"),
-            'Y' to Glyph("_   _", " \\_/ ", "  |  "),
-            'Z' to Glyph("___ ", "  / ", " /__")
-    )
-}
-
-private class Glyph(upperLine: String, mediumLine: String, bottomLine: String) {
+    private val glyphs = mutableMapOf<Char, Glyph>()
 
     init {
-        require(upperLine.length in 1..5) { "Length must be in range from 1 to 5" }
-        require(upperLine.length == mediumLine.length) { "Length of first and second lines must be equal" }
-        require(upperLine.length == bottomLine.length) { "Length of first and third lines must be equal" }
+        Scanner(File(fileName)).use { file ->
+            height = file.nextInt()
+            val glyphNumber = file.nextInt()
+            repeat(glyphNumber) {
+                val character = file.next()
+                val width = file.nextInt()
+                file.nextLine()
+                glyphs[character[0]] = Glyph(Array(height) { file.nextLine() })
+            }
+        }
+        // add whitespace using 'a' glyph width
+        glyphs[' '] = Glyph(Array(height) { " ".repeat(glyphs['a']!!.width) })
+    }
+
+    operator fun get(ch: Char): Glyph = glyphs[ch] ?: throw IllegalStateException("Illegal letter $ch in file $fileName")
+}
+
+private class Glyph(private val lines: Array<String>) {
+
+    init {
+        require(lines.distinctBy { it.length }.size == 1) { "All lines in glyph must be same length" }
     }
 
     val width: Int by lazy { lines[0].length }
-
-    private val lines = arrayOf(upperLine, mediumLine, bottomLine)
 
     operator fun get(i: Int) = lines[i]
 }
